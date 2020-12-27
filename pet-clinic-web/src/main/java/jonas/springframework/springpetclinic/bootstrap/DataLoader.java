7b0def2a4ceac720449fa3f5b8ca50efa6853e58
Jonas Singbo <jonass@wigroup.co.za>
2020-12-27 20:43:56 +0200

    refactor map impl to abstract id generator

--- pet-clinic-web/src/main/java/jonas/springframework/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/jonas/springframework/springpetclinic/bootstrap/DataLoader.java
@@ -21,7 +21,6 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
 //        owner1.setAddress("123 Brickerel");
@@ -31,7 +30,6 @@ public class DataLoader implements CommandLineRunner {
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
 //        owner2.setAddress("123 Brickerel");
@@ -43,7 +41,6 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners....");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
 //        vet1.getSpecialities().add(savedRadiology);
@@ -51,7 +48,6 @@ public class DataLoader implements CommandLineRunner {
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
 //        vet2.getSpecialities().add(savedSurgery);
