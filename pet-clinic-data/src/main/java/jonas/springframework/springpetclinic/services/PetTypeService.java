916d8a5325b289c344ee501991ab7d52d211e5a7
Jonas Singbo <jonass@wigroup.co.za>
2020-12-30 08:25:58 +0200

    added pet type service

---
+++ pet-clinic-data/src/main/java/jonas/springframework/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package jonas.springframework.springpetclinic.services;
+
+import jonas.springframework.springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
