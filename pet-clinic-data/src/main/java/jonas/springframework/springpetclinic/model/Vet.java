e4b2d4831621554108a21c47da7dbdc637fe19bc
Jonas Singbo <jonass@wigroup.co.za>
2021-01-01 08:09:05 +0200

    updated vet to use JPA

--- pet-clinic-data/src/main/java/jonas/springframework/springpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/jonas/springframework/springpetclinic/model/Vet.java
@@ -1,16 +1,22 @@
 package jonas.springframework.springpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person{
-    private Set<Speciality> specialities = new HashSet<>();
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "speciality_id"))
+    private Set<Speciality> specialties = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
-        return specialities;
+        return specialties;
     }
 
     public void setSpecialities(Set<Speciality> specialities) {
-        this.specialities = specialities;
+        this.specialties = specialities;
     }
 }
